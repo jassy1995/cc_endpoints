9f30f7aee0474f79dd53f5c0642e7cd5778bec00
jassy1995 <babatundejoseph85@gmail.com>
2022-03-20 16:47:54 +0100

    add pre message

--- controllers/otp.controller.js
+++ controllers/otp.controller.js
@@ -1,9 +1,70 @@
 require("dotenv").config();
-const api_keys = process.env.API_KEY;
+// const api_keys = process.env.API_KEY;
+const { MESSENGER_TOKEN, api_keys } = process.env;
 const axios = require("axios");
 const winston = require("../loggers");
 
 const validateNumber = require("../validator/otp/otp-validator");
+
+function sendText(phoneNumber, provider, channelId, phone) {
+  if (provider === "web") return;
+
+  let url;
+  let head;
+  let body;
+
+  if (provider == "web") {
+    url = "https://bnpl-chatbot-server.herokuapp.com/direct";
+
+    head = {
+      Authorization: `Bearer ${MESSENGER_TOKEN}`,
+    };
+    body = {
+      phone: "234" + phone.substr(-10).replace(" ", ""),
+      message: "please wait while generating your otp",
+    };
+  } else if (provider == "messengerpeople") {
+    url = "https://api.messengerpeople.dev/messages";
+
+    head = {
+      Authorization: `Bearer ${MESSENGER_TOKEN}`,
+    };
+    body = {
+      recipient: "234" + phone.substr(-10).replace(" ", ""),
+      sender: channelId,
+      payload: {
+        type: "text",
+        text: "please wait while generating your otp",
+      },
+    };
+  } else if (provider == "messagebird") {
+    url = "https://conversations.messagebird.com/v1/send";
+
+    head = {
+      Authorization: `${MESSENGEBIRD_TOKEN}`,
+    };
+    body = {
+      to: "+234" + phone.substr(-10).replace(" ", ""),
+      from: channelId,
+      type: "text",
+      content: {
+        text: "please wait while generating your otp",
+        disableUrlPreview: false,
+      },
+    };
+  }
+
+  return new Promise((resolve) => {
+    axios
+      .post(url, body, { headers: head })
+      .then(async (response) => {
+        resolve(response.data);
+      })
+      .catch((err) => {
+        return "error";
+      });
+  });
+}
 
 exports.generateOtp = async (req, res, next) => {
   const { error, value } = validateNumber(req.body);
@@ -11,6 +72,8 @@ exports.generateOtp = async (req, res, next) => {
     return res.status(400).json({ message: error.details[0].message });
   }
 
+  const { phoneNumber, provider, channelId, phone } = req.body;
+  sendText(phoneNumber, provider, channelId, phone);
   let config = {
     method: "post",
     url: "https://mobile.creditclan.com/api/v3/lender/generate/otp",
@@ -18,7 +81,7 @@ exports.generateOtp = async (req, res, next) => {
       "X-API-KEY": api_keys,
       "Content-Type": "application/json",
     },
-    data: { phone: req.body.phone },
+    data: { phone: phone },
   };
   let response = await axios(config);
   winston.info(response.data.message);
