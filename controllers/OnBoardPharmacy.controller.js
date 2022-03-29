010d0abdd06c92ff7a06d6959ceac9ee9b6de54a
jassy1995 <babatundejoseph85@gmail.com>
2022-03-29 10:34:51 +0100

    changed employee model to pharmacy

--- controllers/OnBoardPharmacy.controller.js
+++ controllers/OnBoardPharmacy.controller.js
@@ -9,10 +9,10 @@ exports.RegisterPharmacy = async (req, res) => {
   //      "channelId":"d599d756-7e5c-4514-8cdf-04ab47955b1e"
   //      "phone" : "07035280592"
   // }
-  const pharmacy = await Pharmacy.create({
-    name: req.body.name,
-    email: req.body.email,
-  });
+  // const pharmacy = await Pharmacy.create({
+  //   name: req.body.name,
+  //   email: req.body.email,
+  // });
 
   return res.status(200).json({
     message: "pharmacy created work",
