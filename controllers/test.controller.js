23188bd3bd868173dc367ec4e313ee9dfac02af7
jassy1995 <babatundejoseph85@gmail.com>
2022-03-29 08:43:30 +0100

    added the email

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -3,6 +3,7 @@ const { Employee } = require("../models");
 exports.TestFunction = async (req, res) => {
   const employee = await Employee.create({
     name: "John",
+    email: "john@example.com",
   });
 
   return res.status(200).json({
